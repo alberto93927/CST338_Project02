299207673244a4b37413a9cbabf5714097744ad3
Logan Druley <logan.druley@gmail.com>
2024-05-02 00:15:56 -0700

    Switched to lambdas & fixed warnings

--- app/src/main/java/com/daclink/fastfood/ButtonFragment.java
+++ app/src/main/java/com/daclink/fastfood/ButtonFragment.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.util.Log;
@@ -15,7 +14,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 
 public class ButtonFragment extends Fragment {
@@ -43,24 +41,18 @@ public class ButtonFragment extends Fragment {
         button2 = view.findViewById(R.id.checkout_button);
 
         // Set onClick listeners for each button
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle button1 click
-                AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
-                CartFragment cartFragment = new CartFragment();
-                activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, cartFragment).addToBackStack(null).commit();
-            }
+        button1.setOnClickListener(v -> {
+            // Handle button1 click
+            AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
+            CartFragment cartFragment = new CartFragment();
+            activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, cartFragment).addToBackStack(null).commit();
         });
 
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle button2 click
-                AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
-                CheckoutFragment checkoutFragment = new CheckoutFragment();
-                activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, checkoutFragment).addToBackStack(null).commit();
-            }
+        button2.setOnClickListener(v -> {
+            // Handle button2 click
+            AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
+            CheckoutFragment checkoutFragment = new CheckoutFragment();
+            activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, checkoutFragment).addToBackStack(null).commit();
         });
         observeCurrentFragment();
         return view;
@@ -77,98 +69,76 @@ public class ButtonFragment extends Fragment {
     // Observe changes in the current fragment
     // This could probably be implemented in a more modular way but time is a finite resourced
     private void observeCurrentFragment() {
-        sharedViewModel.getCurrentFragmentTag().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(String tag) {
-                // Update button behavior in the second container based on the current fragment
-                if (tag.equals("Browse")) {
+        sharedViewModel.getCurrentFragmentTag().observe(getViewLifecycleOwner(), tag -> {
+            // Update button behavior in the second container based on the current fragment
+            switch (tag) {
+                case "Browse":
                     Log.d("FragmentTag", "Browse");
                     button1.setText("View Cart");
-                    button1.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
-                            CartFragment cartFragment = new CartFragment();
-                            activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, cartFragment).addToBackStack(null).commit();
-                        }
+                    button1.setOnClickListener(v -> {
+                        AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
+                        CartFragment cartFragment = new CartFragment();
+                        activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, cartFragment).addToBackStack(null).commit();
                     });
 
                     button2.setText("Checkout");
-                    button2.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
-                            CheckoutFragment checkoutFragment = new CheckoutFragment();
-                            activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, checkoutFragment).addToBackStack(null).commit();
-                        }
+                    button2.setOnClickListener(v -> {
+                        AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
+                        CheckoutFragment checkoutFragment = new CheckoutFragment();
+                        activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, checkoutFragment).addToBackStack(null).commit();
                     });
-                } else if (tag.equals("Cart")) {
+                    break;
+                case "Cart":
                     // Update button behavior for Fragment2
                     Log.d("FragmentTag", "Cart");
                     button1.setText("Browse");
-                    button1.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
-                            ProductListFragment productListFragment = new ProductListFragment();
-                            activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, productListFragment).addToBackStack(null).commit();
-                        }
+                    button1.setOnClickListener(v -> {
+                        AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
+                        ProductListFragment productListFragment = new ProductListFragment();
+                        activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, productListFragment).addToBackStack(null).commit();
                     });
 
                     button2.setText("Checkout");
-                    button2.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
-                            CheckoutFragment checkoutFragment = new CheckoutFragment();
-                            activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, checkoutFragment).addToBackStack(null).commit();
-                        }
+                    button2.setOnClickListener(v -> {
+                        AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
+                        CheckoutFragment checkoutFragment = new CheckoutFragment();
+                        activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, checkoutFragment).addToBackStack(null).commit();
                     });
-                } else if (tag.equals("Checkout")) {
+                    break;
+                case "Checkout":
                     // Update button behavior for Fragment2
                     Log.d("FragmentTag", "Checkout");
                     button1.setText("Browse");
-                    button1.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
-                            ProductListFragment productListFragment = new ProductListFragment();
-                            activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, productListFragment).addToBackStack(null).commit();
-                        }
+                    button1.setOnClickListener(v -> {
+                        AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
+                        ProductListFragment productListFragment = new ProductListFragment();
+                        activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, productListFragment).addToBackStack(null).commit();
                     });
 
                     button2.setText("View Cart");
-                    button2.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
-                            CartFragment cartFragment = new CartFragment();
-                            activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, cartFragment).addToBackStack(null).commit();
-                        }
+                    button2.setOnClickListener(v -> {
+                        AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
+                        CartFragment cartFragment = new CartFragment();
+                        activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, cartFragment).addToBackStack(null).commit();
                     });
-                } else if (tag.equals("Product")) {
+                    break;
+                case "Product":
                     // Update button behavior for Fragment2
                     Log.d("FragmentTag", "Product");
                     button1.setText("Browse");
-                    button1.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
-                            ProductListFragment productListFragment = new ProductListFragment();
-                            activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, productListFragment).addToBackStack(null).commit();
-                        }
+                    button1.setOnClickListener(v -> {
+                        AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
+                        ProductListFragment productListFragment = new ProductListFragment();
+                        activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, productListFragment).addToBackStack(null).commit();
                     });
 
                     button2.setText("View Cart");
-                    button2.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
-                            CartFragment cartFragment = new CartFragment();
-                            activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, cartFragment).addToBackStack(null).commit();
-                        }
+                    button2.setOnClickListener(v -> {
+                        AppCompatActivity activity = (AppCompatActivity) unwrap(v.getContext());
+                        CartFragment cartFragment = new CartFragment();
+                        activity.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, cartFragment).addToBackStack(null).commit();
                     });
-                }
+                    break;
             }
         });
     }
