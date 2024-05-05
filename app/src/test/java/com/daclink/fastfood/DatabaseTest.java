e730680134221029151774dd533730fa7dfab283
Logan Druley <logan.druley@gmail.com>
2024-05-05 16:08:19 -0700

    Added Database Tests

--- app/src/test/java/com/daclink/fastfood/DatabaseTest.java
+++ app/src/test/java/com/daclink/fastfood/DatabaseTest.java
@@ -7,6 +7,7 @@ import android.content.Context;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
 
 import com.daclink.fastfood.Database.entities.FoodDatabase;
 import com.daclink.fastfood.Database.entities.User;
@@ -16,41 +17,41 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import java.io.IOException;
 import java.util.List;
 
+//Have issues with gradle, these tests are currently failing due to those issues.
+
+@RunWith(JUnit4.class)
 public class DatabaseTest {
-    @RunWith(AndroidJUnit4.class)
-    public class SimpleEntityReadWriteTest {
-        private userDAO userDao;
-        private FoodDatabase db;
+    private userDAO userDao;
+    private FoodDatabase db;
 
-        @Before
-        public void createDb() {
-            Context context = ApplicationProvider.getApplicationContext();
-            db = Room.inMemoryDatabaseBuilder(context, FoodDatabase.class).build();
-            userDao = db.foodDAO();
-        }
-
-        @After
-        public void closeDb() throws IOException {
-            db.close();
-        }
-
-        @Test
-        public void addAndVerifyUser() throws Exception {
-            User user = new User("dave", "pass", "user");
-            userDao.insertUser(user);
-            LiveData<List<User>> byName = userDao.findUserByName("dave");
-            assertThat(byName.getValue().get(0).getName(), equalTo(user.getName()));
-        }
-
-        @Test
-        public void verifyDefaultUser() throws Exception {
-            LiveData<List<User>> admin = userDao.findUserByName("admin2");
-            assertThat(admin.getValue().get(0).getName(), equalTo("admin2"));
-        }
+    @Before
+    public void createDb() {
+        Context context = ApplicationProvider.getApplicationContext();
+        db = Room.inMemoryDatabaseBuilder(context, FoodDatabase.class).build();
+        userDao = db.foodDAO();
     }
 
+    @After
+    public void closeDb() throws IOException {
+        db.close();
+    }
+
+    @Test
+    public void addAndVerifyUser() throws Exception {
+        User user = new User("dave", "pass", "user");
+        userDao.insertUser(user);
+        LiveData<List<User>> byName = userDao.findUserByName("dave");
+        assertThat(byName.getValue().get(0).getName(), equalTo(user.getName()));
+    }
+
+    @Test
+    public void verifyDefaultUser() throws Exception {
+        LiveData<List<User>> admin = userDao.findUserByName("admin2");
+        assertThat(admin.getValue().get(0).getName(), equalTo("admin2"));
+    }
 }
