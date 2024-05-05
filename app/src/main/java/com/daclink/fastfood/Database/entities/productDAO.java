9aa38c3e85e766ecb61d8604bca07dcd09465b16
Logan Druley <logan.druley@gmail.com>
2024-05-05 15:41:05 -0700

    Added Database Tests

--- app/src/main/java/com/daclink/fastfood/Database/entities/productDAO.java
+++ app/src/main/java/com/daclink/fastfood/Database/entities/productDAO.java
@@ -1,40 +1,40 @@
-package com.daclink.fastfood.Database.entities;
+    package com.daclink.fastfood.Database.entities;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.room.Dao;
-import androidx.room.Delete;
-import androidx.room.Insert;
-import androidx.room.OnConflictStrategy;
-import androidx.room.Query;
-import androidx.room.Update;
+    import androidx.lifecycle.LiveData;
+    import androidx.lifecycle.MutableLiveData;
+    import androidx.room.Dao;
+    import androidx.room.Delete;
+    import androidx.room.Insert;
+    import androidx.room.OnConflictStrategy;
+    import androidx.room.Query;
+    import androidx.room.Update;
 
-import java.util.List;
+    import java.util.List;
 
-@Dao
-public interface productDAO {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insertProduct(Product... products);
+    @Dao
+    public interface productDAO {
+        @Insert(onConflict = OnConflictStrategy.REPLACE)
+        void insertProduct(Product... products);
 
-    @Delete
-    void deleteProduct(Product product);
+        @Delete
+        void deleteProduct(Product product);
 
-    @Update
-    void updateProduct(Product product);
+        @Update
+        void updateProduct(Product product);
 
-    @Query("DELETE from " + FoodDatabase.productTable)
-    void deleteAll();
+        @Query("DELETE from " + FoodDatabase.productTable)
+        void deleteAll();
 
-    @Query("Select * from " + FoodDatabase.productTable + " ORDER BY id")
-    LiveData<List<Product>> getAllProducts();
+        @Query("Select * from " + FoodDatabase.productTable + " ORDER BY id")
+        LiveData<List<Product>> getAllProducts();
 
-    @Query("Select * from " + FoodDatabase.productTable + " WHERE name LIKE :search")
-    LiveData<List<Product>> searchProductByName(String search);
+        @Query("Select * from " + FoodDatabase.productTable + " WHERE name LIKE :search")
+        LiveData<List<Product>> searchProductByName(String search);
 
-    @Query("Select * from " + FoodDatabase.productTable + " WHERE id = :id")
-    List<Product> findProductByID(int id);
+        @Query("Select * from " + FoodDatabase.productTable + " WHERE id = :id")
+        List<Product> findProductByID(int id);
 
-    @Query("SELECT * FROM " + FoodDatabase.productTable + " WHERE id IN (:productIds)")
-    LiveData<List<Product>> findProductsByCart(List<Integer> productIds);
-}
+        @Query("SELECT * FROM " + FoodDatabase.productTable + " WHERE id IN (:productIds)")
+        LiveData<List<Product>> findProductsByCart(List<Integer> productIds);
+    }
 
---
+++ app/src/test/java/com/daclink/fastfood/DatabaseTest.java
@@ -0,0 +1,56 @@
+package com.daclink.fastfood;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Room;
+
+import com.daclink.fastfood.Database.entities.FoodDatabase;
+import com.daclink.fastfood.Database.entities.User;
+import com.daclink.fastfood.Database.entities.userDAO;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.List;
+
+public class DatabaseTest {
+    @RunWith(AndroidJUnit4.class)
+    public class SimpleEntityReadWriteTest {
+        private userDAO userDao;
+        private FoodDatabase db;
+
+        @Before
+        public void createDb() {
+            Context context = ApplicationProvider.getApplicationContext();
+            db = Room.inMemoryDatabaseBuilder(context, FoodDatabase.class).build();
+            userDao = db.foodDAO();
+        }
+
+        @After
+        public void closeDb() throws IOException {
+            db.close();
+        }
+
+        @Test
+        public void addAndVerifyUser() throws Exception {
+            User user = new User("dave", "pass", "user");
+            userDao.insertUser(user);
+            LiveData<List<User>> byName = userDao.findUserByName("dave");
+            assertThat(byName.getValue().get(0).getName(), equalTo(user.getName()));
+        }
+
+        @Test
+        public void verifyDefaultUser() throws Exception {
+            LiveData<List<User>> admin = userDao.findUserByName("admin2");
+            assertThat(admin.getValue().get(0).getName(), equalTo("admin2"));
+        }
+    }
+
+}
